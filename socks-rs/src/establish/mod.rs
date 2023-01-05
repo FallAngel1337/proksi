773f1dffc6fadb514ffc5676a1e60779c60dfd8d
FallAngel1337 <fallangel@protonmail.com>
2023-01-05 09:29:04 -0300

    Add the `EstablishRequest` struct

--- socks-rs/src/establish/mod.rs
+++ socks-rs/src/establish/mod.rs
@@ -2,3 +2,12 @@
 //! This moodule contains the struct that describes the connection establish request
 //! that need to be sent to the SOCKS server.
 
+pub(crate) mod methods;
+use methods::Methods;
+
+/// The request packet
+pub struct EstablishRequest {
+    version: u8,
+    nmethods: u8,
+    methods: Vec<Methods>
+}
