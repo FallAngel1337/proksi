c3e4b88301647a6804aaae519a5f275e9f418d30
FallAngel1337 <fallangel@protonmail.com>
2023-01-05 09:38:21 -0300

    Change from `Vec<Methods>` to `&[Methods]` and add the constructor

--- socks-rs/src/establish/mod.rs
+++ socks-rs/src/establish/mod.rs
@@ -4,10 +4,22 @@
 
 pub(crate) mod methods;
 use methods::Methods;
+use crate::SOCKS_VERSION;
 
 /// The request packet
-pub struct EstablishRequest {
+pub struct EstablishRequest<'a> {
     version: u8,
     nmethods: u8,
-    methods: Vec<Methods>
+    methods: &'a [Methods]
+}
+
+impl<'a> EstablishRequest<'a> {
+    /// Constructs a new connection establish request
+    pub fn new(methods: &'a [Methods]) -> Self {
+        Self {
+            version: SOCKS_VERSION,
+            nmethods: methods.len() as u8,
+            methods
+        }
+    }
 }
