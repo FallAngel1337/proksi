9862cdcf4b5d363f7650b5cf5fe1eab8ca93ae83
FallAngel1337 <fallangel@protonmail.com>
2023-01-05 16:08:31 -0300

    Add the necessary imports

--- socks-rs/src/server/mod.rs
+++ socks-rs/src/server/mod.rs
@@ -3,14 +3,19 @@
 //! TODO: Add a better description
 
 use std::net::{ToSocketAddrs, SocketAddr};
-use std::io;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use std::sync::Arc;
+use std::io;
 use crate::{
     establish::{Method, EstablishRequest, EstablishResponse},
-    SOCKS_VERSION,
+    requests::{
+        Request,
+        command::Command,
+        addr_type::AddrType
+    },
     utils::*,
+    SOCKS_VERSION,
 };
 
 /// The `Server` struct that holds the
@@ -18,7 +23,7 @@ use crate::{
 #[derive(Debug, Clone)]
 pub struct Server {
     version: u8,
-    supported_methods: Vec<Method>,
+    supported_methods: Arc<Vec<Method>>,
     addr: SocketAddr,
 }
 
@@ -32,7 +37,7 @@ impl Server {
         Ok(
             Self {
                 version: SOCKS_VERSION,
-                supported_methods: supported_methods.to_vec(),
+                supported_methods: Arc::new(supported_methods.to_vec()),
                 addr
             }
         )
@@ -41,21 +46,27 @@ impl Server {
     /// Start the server and listen for new connections
     pub async fn start(self) -> io::Result<()> {
         let listener = TcpListener::bind(&self.addr).await?;
-        let supported_methods = Arc::new(self.supported_methods);
+        let supported_methods = self.supported_methods.clone();
 
         loop {
             let (stream, _) = listener.accept().await?;
             let delivery = Arc::new(Delivery::new(stream));
             let methods = supported_methods.clone();
-            
+
+            if Self::handle_establish(delivery.clone(), methods).await.is_err() {
+                break Ok(());
+            }
+
             tokio::spawn(async move {
-                Self::establish(delivery.clone(), methods.clone()).await.unwrap();
+                loop {
+                    Self::handle_requests(delivery.clone()).await.unwrap();
+                }
             });
         }
     }
 
-    async fn establish(delivery: Arc<Delivery>, methods: Arc<Vec<Method>>) -> io::Result<()> {
-        // let estbl_req = recv::<EstablishRequest>(stream, &mut Vec::with_capacity(100)).await?;
+    // TODO: implement the user and password authentication if selected
+    async fn handle_establish(delivery: Arc<Delivery>, methods: Arc<Vec<Method>>) -> io::Result<()> {
         let estbl_req = delivery.recv::<EstablishRequest>(&mut Vec::with_capacity(100)).await?;
                 
         if methods.iter().any(
@@ -66,10 +77,18 @@ impl Server {
             }
         } else {
             delivery.send(EstablishResponse::new(Method::NoAcceptableMethods)).await?;
+            return Err(io::Error::new(io::ErrorKind::ConnectionAborted, "NO ACCEPTABLE METHODS"))
         }
 
         Ok(())
     }
+
+    async fn handle_requests(delivery: Arc<Delivery>) -> io::Result<()> {
+        let requets = delivery.recv::<Request>(&mut Vec::with_capacity(100)).await?;
+        delivery.send(requets).await?;
+        Ok(())
+    }
+
 }
 
 impl Default for Server {
@@ -85,8 +104,8 @@ mod tests {
     use tokio::time::{self, Duration};
 
     #[tokio::test]
-    async fn server_test() {
-        let server = Server::new("127.0.0.1:8000", &[Method::UsernamePassword]).unwrap();
+    async fn server_establish_test() {
+        let server = Server::new("127.0.0.1:1080", &[Method::NoAuthenticationRequired]).unwrap();
         let addr = server.addr;
 
         let hdl = tokio::spawn(async move { server.start().await.unwrap() });
@@ -97,15 +116,17 @@ mod tests {
         let estbl_req = EstablishRequest::new(&[Method::NoAuthenticationRequired, Method::UsernamePassword]);
         delivery.send(estbl_req).await.unwrap();
         let data = delivery.recv::<EstablishResponse>(&mut Vec::new()).await.unwrap();
+
+        assert_ne!(*data.method(), Method::NoAcceptableMethods);
 
         hdl.abort();
         time::sleep(Duration::from_secs(1)).await;
         assert!(hdl.is_finished());
     }
-    
+
     #[tokio::test]
-    async fn default_server_test() {
-        let server = Server::default();
+    async fn server_request_test() {
+        let server = Server::new("127.0.0.1:1081", &[Method::NoAuthenticationRequired]).unwrap();
         let addr = server.addr;
 
         let hdl = tokio::spawn(async move { server.start().await.unwrap() });
@@ -116,6 +137,14 @@ mod tests {
         let estbl_req = EstablishRequest::new(&[Method::NoAuthenticationRequired, Method::UsernamePassword]);
         delivery.send(estbl_req).await.unwrap();
         let data = delivery.recv::<EstablishResponse>(&mut Vec::new()).await.unwrap();
+
+        assert_ne!(*data.method(), Method::NoAcceptableMethods);
+
+        let request = Request::new(Command::Connect, AddrType::DomainName, "localhost".as_bytes(), 8080);
+        delivery.send::<Request>(request).await.unwrap();
+        let data = delivery.recv::<Request>(&mut Vec::with_capacity(100)).await.unwrap();
+
+        println!("{data:?}");
 
         hdl.abort();
         time::sleep(Duration::from_secs(1)).await;
