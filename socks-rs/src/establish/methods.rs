5246904cbe0abbc77320f2e025a059f8ad10f82c
FallAngel1337 <fallangel@protonmail.com>
2023-01-05 09:28:32 -0300

    Add the `Methods` enum

---
+++ socks-rs/src/establish/methods.rs
@@ -0,0 +1,17 @@
+//! # Methods
+//! The methods that can be sent to the SOCKS server
+
+///The values currently defined for METHOD are:
+/// 
+/// * X'00' NO AUTHENTICATION REQUIRED (OK)
+/// * X'01' GSSAPI (OK)
+/// * X'02' USERNAME/PASSWORD (OK)
+/// * X'03' to X'7F' IANA ASSIGNED
+/// * X'80' to X'FE' RESERVED FOR PRIVATE METHODS
+/// * X'FF' NO ACCEPTABLE METHODS (OK)
+pub enum Methods {
+    NoAuthenticationRequired = 0x0,
+    Gssapi = 0x1,
+    UsernamePassword = 0x2,
+    NoAcceptableMethods = 0xff,
+}
