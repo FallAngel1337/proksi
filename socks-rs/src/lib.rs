6b3f8358e4f8ff0b2d6ff97581623988dbdc1d6b
FallAngel1337 <fallangel@protonmail.com>
2023-01-16 09:59:32 -0300

    Fix markdown

--- socks-rs/src/lib.rs
+++ socks-rs/src/lib.rs
@@ -1,8 +1,7 @@
 #![warn(missing_docs)]
 
 //! # A pure Rust implementation of SOCKS 5 protocol
-//! according to [`RFC 1928`](https://datatracker.ietf.org/doc/html/rfc1928)
-//! TODO: Add a better description and items
+//! According to [`RFC 1928`](https://datatracker.ietf.org/doc/html/rfc1928)
 
 /// The SOCKS protocol version
 pub const SOCKS_VERSION: u8 = 0x5;
