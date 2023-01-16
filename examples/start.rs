72b87045001e34ec7100af6970aa634e1ccb64ce
FallAngel1337 <fallangel@protonmail.com>
2023-01-16 08:22:13 -0300

    Add examples

---
+++ examples/start.rs
@@ -0,0 +1,16 @@
+use std::{fs::File, io::Read};
+use proksi::Server;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = File::open("./examples/server_config.json")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let server: Server = serde_json::from_str(&contents)?;
+
+    println!("Listening at {}", server.addr);
+    server.start().await?;
+
+    Ok(())
+}
